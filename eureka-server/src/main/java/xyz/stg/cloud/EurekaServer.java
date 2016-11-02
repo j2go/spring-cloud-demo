14138d153321f61b82c85622a56b5654c92d6165
tiangao <tiangao1102@163.com>
2016-11-03 00:47:43 +0800

    add eureka server

---
+++ eureka-server/src/main/java/xyz/stg/cloud/EurekaServer.java
@@ -0,0 +1,16 @@
+package xyz.stg.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Admin on 2016/11/2.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
