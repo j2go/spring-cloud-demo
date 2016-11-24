e7201e4b22d170131a55aac6dde2f25ea268e4ef
shitg <tiangao1102@163.com>
2016-11-24 15:56:51 +0800

    增加一些安全配置

--- rest-customer-demo/src/main/java/xyz/stg/cloud/Customer1.java
+++ rest-customer-demo/src/main/java/xyz/stg/cloud/Customer1.java
@@ -1,31 +1,27 @@
 package xyz.stg.cloud;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by shitiangao on 2016/10/31.
+ * Created by shitiangao on 2016/11/4.
  */
 @SpringBootApplication
-@RefreshScope
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
-public class Client {
-
-    @Value("${hello.answer}")
-    String foo;
+public class Customer1 {
 
     @RequestMapping("/hello")
     public String hello() {
-        return foo;
+        return "client1";
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Client.class, args);
+        SpringApplication.run(Customer1.class);
     }
 }
